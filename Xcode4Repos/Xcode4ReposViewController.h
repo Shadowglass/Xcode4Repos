88def8ebcc44aa1c60f882393f58410396d841a9
Kerry Brockway <Kerry@kerry-brockways-macbook-pro.local>
2011-03-17 20:41:45 -0400

    committing from xcode 4

--- Xcode4Repos/Xcode4ReposViewController.h
+++ Xcode4Repos/Xcode4ReposViewController.h
@@ -5,6 +5,7 @@
 //  Created by Kerry Brockway on 3/17/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
+//  This file has changed (made this comment to make a point in the demo)
 
 #import <UIKit/UIKit.h>
 
